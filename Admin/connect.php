df80026ed2a7e6235c825a46e01d7315196fa7ce
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-07 06:07:20 -0700

    Add files via upload

---
+++ Admin/connect.php
@@ -0,0 +1,19 @@
+<?php
+    $dsn = "mysql:host=localhost;dbname=WorkShop"; //Data Source Name 'Not Make space'
+    $user = "root";
+    $pass ='';
+    //UTF-8
+    $option = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    );
+    //
+    try{
+       $con = new PDO($dsn,$user,$pass,$option);
+       $con->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+        echo "Connect sucessfully ..";
+    }
+    catch(PDOException $e)
+    {
+        echo "is Faild" . $e->getMessage();
+    }
+?>
