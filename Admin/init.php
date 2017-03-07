3cb27118db9c45d30911bf7d00df4486f9c88891
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-07 06:09:07 -0700

    Add files via upload

---
+++ Admin/init.php
@@ -0,0 +1,12 @@
+<?php
+    include "connect.php";
+ //Routes
+    $tpl = "Includes/Templetes/";
+    $lan = "Includes/Languages/";
+    $fun = "Includes/Function/";
+    $css = "Layout/CSS/";
+    $js =  "Layout/JS/";
+ //Important Files arrangemnt is important
+    include $fun . "function.php";
+    include $tpl . "header.php";
+?>
