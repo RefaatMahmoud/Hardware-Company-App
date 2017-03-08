c3aa5d1bf22a553374bb93ec14616b2659744f2b
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-08 12:34:59 -0700

    Update arabic.php

--- Admin/include/Languages/arabic.php
+++ Admin/include/Languages/arabic.php
@@ -1,1 +1,20 @@
 
+<?php
+
+function lang($phares)
+{
+    static $lang = array(
+//    "MESSAGE" => "مرحبا",
+//    "ADMIN" => "صاحب الموقع"
+    "Home-Admin"    => "لوحة التحكم",
+    "ADMIN"         => "المسؤل عن الموقع",
+    "Categories"    =>"الاقسام",
+    "ITEMS"        =>"السلع",
+    "Statistics"    =>"الاحصائيات",
+    "Members"      =>"الاعضاء",
+    "Logs"          =>"الدخول"
+    );
+    return $lang[$phares];
+}
+
+?>
