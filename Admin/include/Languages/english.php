7bc424f79a2943c432e8fed0a09db3cf6ccbf456
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-08 12:19:50 -0700

    Update Languages

--- Admin/include/Languages/english.php
+++ Admin/include/Languages/english.php
@@ -1,1 +1,15 @@
-
+<?php
+function lang($phares)
+{
+    static $lang = array(
+    "Home-Admin"    => "Admin-Area",
+    "ADMIN"         => "Adminstrator",
+    "Categories"    =>"Categories",
+    "ITEMS"        =>"Items ",
+    "Statistics"    =>"Statistics ",
+    "Members"      =>"Members ",
+    "Logs"          =>"Logs"
+    );
+    return $lang[$phares];
+}
+?>
