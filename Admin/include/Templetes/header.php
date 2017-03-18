249361ad8cd73847209d54657c7244242d6b39a8
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-17 19:40:29 -0700

    update templates

--- Admin/include/Templetes/header.php
+++ Admin/include/Templetes/header.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php getTitle(); ?></title>
+        <link rel="stylesheet" href="<?php echo $css ; ?>bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo $css ; ?>font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo $css ; ?>backend.css">
+    </head>
+<body>
