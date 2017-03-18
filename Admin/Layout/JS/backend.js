e77bfea6dffbcd517cf0e56de65fbc7e1ff0200d
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-17 20:18:43 -0700

    update backend.js

--- Admin/Layout/JS/backend.js
+++ Admin/Layout/JS/backend.js
@@ -1,31 +1,3 @@
-
-/*Strart Astriex*/
-$('input').each(function(){
-   if($(this).attr("required") === 'required')
-       {
-           $(this).after('<span class="asterix">*</span>');
-       }
-});
-/*End Asterix*/
-
-
-/*Strart eye*/
-var passfield = $('.password');
-$('.show-pass').hover(function(){
-    passfield.attr('type','text');
-},
-//when leave by mouse
-function(){
-    passfield.attr('type','password');
-});
-/*End eye*/    
-
-
-/*Strart in member page Delete confirm*/
-$('.confirm').click(function(){
-    return confirm("Are you sure about delete this record!!");
-});
-/* End in member page Delete confirm*/
 /*Strart Astriex*/
 $('input').each(function(){
    if($(this).attr("required") === 'required')
