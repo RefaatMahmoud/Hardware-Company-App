668e07ccc98cb573d28807311cc17e948dd80029
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-17 19:35:43 -0700

    update php scripts

--- Admin/connect.php
+++ Admin/connect.php
@@ -10,7 +10,7 @@
     try{
        $con = new PDO($dsn,$user,$pass,$option);
        $con->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
-        echo "Connect sucessfully ..";
+       // echo "Connect sucessfully ..";
     }
     catch(PDOException $e)
     {
