6e675120d7b8f00d7f661fc8b38490b4dcbc4761
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-03-18 07:04:44 -0700

    update Templetes

--- Admin/include/Templetes/footer.php
+++ Admin/include/Templetes/footer.php
@@ -1,1 +1,5 @@
-
+</body>
+<script src="<?php echo $js ; ?>jquery-3.1.1.min.js"></script>
+<script src="<?php echo $js ; ?>bootstrap.min.js"></script>
+<script src="<?php echo $js ; ?>backend.js"></script>
+</html>
--- Admin/include/Templetes/navbar.php
+++ Admin/include/Templetes/navbar.php
@@ -1,5 +1,3 @@
-
-
 <nav class="navbar navbar-inverse">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -22,7 +20,7 @@
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin<span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['username']?><span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="members.php?do=edit&userid=<?php echo $_SESSION['ID'] ?>">Edit Profile</a></li>
             <li><a href="#">Setting</a></li>
@@ -33,38 +31,3 @@
     </div>
   </div>
 </nav>
-
-<nav class="navbar navbar-inverse">
-  <div class="container">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-nav" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="dashboard.php"><?php echo lang("Home-Admin");?></a>
-    </div>
-    <div class="collapse navbar-collapse" id="app-nav">
-      <ul class="nav navbar-nav">
-        <li><a href="category.php"><?php echo lang('Categories');?></a></li>
-        <li><a href="Items.php"><?php echo lang('ITEMS');?></a></li>
-        <li><a href="dashboard.php"><?php echo lang('Statistics');?></a></li>
-        <li><a href="members.php"><?php echo lang('Members');?></a></li>
-        <li><a href="#"><?php echo lang('Logs');?></a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="members.php?do=edit&userid=<?php echo $_SESSION['ID'] ?>">Edit Profile</a></li>
-            <li><a href="#">Setting</a></li>
-            <li><a href="logout.php">Log out</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
