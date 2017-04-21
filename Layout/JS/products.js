fe3c87ce55c96d51608c0344f5bd79d9e085d8d5
yasmine-ghazy <yasmine.ghazyfciscu@gmail.com>
2017-04-21 15:40:15 +0200

    Add files via upload

---
+++ Layout/JS/products.js
@@ -0,0 +1,141 @@
+
+
+window.onload = function ()
+{
+    slider = document.getElementById("slider");
+  
+    document.getElementById("slider").onchange = function () {
+        document.getElementById("price").innerHTML = document.getElementById("slider").value;
+    }
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+$(document).ready(function () {
+
+
+    var modal = document.getElementById('myModal');
+    var current;
+    $(".cart-img").click(function () {
+
+        current = $(this);
+        modal.style.display = "block";
+    });
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    $(function () {
+        $('form.require-validation').bind('submit', function (e) {
+            var $form = $(e.target).closest('form'),
+                inputSelector = ['input[type=email]', 'input[type=password]',
+                                 'input[type=text]', 'input[type=file]',
+                                 'textarea'].join(', '),
+                $inputs = $form.find('.required').find(inputSelector),
+                $errorMessage = $form.find('div.error'),
+                valid = true;
+
+            $errorMessage.addClass('hide');
+            $('.has-error').removeClass('has-error');
+            $inputs.each(function (i, el) {
+                var $input = $(el);
+                if ($input.val() === '') {
+                    $input.parent().addClass('has-error');
+                    $errorMessage.removeClass('hide');
+                    e.preventDefault(); // cancel on first error
+                }
+            });
+        });
+    });
+
+    /*******Form validation */
+    $(function () {
+        $('form.require-validation').bind('submit', function (e) {
+            var $form = $(e.target).closest('form'),
+                inputSelector = ['input[type=email]', 'input[type=password]',
+                                 'input[type=text]', 'input[type=file]',
+                                 'textarea'].join(', '),
+                $inputs = $form.find('.required').find(inputSelector),
+                $errorMessage = $form.find('div.error'),
+                valid = true;
+
+            $errorMessage.addClass('hide');
+            $('.has-error').removeClass('has-error');
+            $inputs.each(function (i, el) {
+                var $input = $(el);
+                if ($input.val() === '') {
+                    $input.parent().addClass('has-error');
+                    $errorMessage.removeClass('hide');
+                    e.preventDefault(); // cancel on first error
+                }
+            });
+        });
+    });
+
+    $(function () {
+        var $form = $("#payment-form");
+
+        $form.on('submit', function (e) {
+            if (!$form.data('cc-on-file')) {
+                e.preventDefault();
+                Stripe.setPublishableKey($form.data('stripe-publishable-key'));
+                Stripe.createToken({
+                    number: $('.card-number').val(),
+                    cvc: $('.card-cvc').val(),
+                    exp_month: $('.card-expiry-month').val(),
+                    exp_year: $('.card-expiry-year').val()
+                }, stripeResponseHandler);
+            }
+        });
+
+        function stripeResponseHandler(status, response) {
+            if (response.error) {
+                $('.error')
+                  .removeClass('hide')
+                  .find('.alert')
+                  .text(response.error.message);
+            } else {
+                // token contains id, last4, and card type
+                var token = response['id'];
+                // insert the token into the form so it gets submitted to the server
+                $form.find('input[type=text]').empty();
+                $form.append("<input type='hidden' name='reservation[stripe_token]' value='" + token + "'/>");
+                $form.get(0).submit();
+            }
+        }
+    })
+    function hidelist() {
+        document.getElementById('list')
+        if (list.style.display == 'none')
+            list.style.display = 'block';
+        else
+            list.style.display = 'none';
+    }
+
+
+
+});
+
+
+
+
