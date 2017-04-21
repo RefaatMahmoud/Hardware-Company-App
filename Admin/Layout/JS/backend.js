bd906d8bc45b07aee8ce97d05d4a869c2790e5fb
RefaatMahmoud <refaat.aish.fciscu1@hotmail.com>
2017-04-21 15:14:56 -0700

    update in backend.js

--- Admin/Layout/JS/backend.js
+++ Admin/Layout/JS/backend.js
@@ -7,7 +7,6 @@ $('input').each(function(){
 });
 /*End Asterix*/
 
-
 /*Strart eye*/
 var passfield = $('.password');
 $('.show-pass').hover(function(){
@@ -25,3 +24,18 @@ $('.confirm').click(function(){
     return confirm("Are you sure about delete this record!!");
 });
 /* End in member page Delete confirm*/
+
+/*start in Dashboard plus*/
+$('.latest-element').click(function(){
+    $(this).toggleClass('selected').parent().next('.panel-body').fadeToggle(100);
+    if($(this).hasClass('selected'))
+        {
+            $(this).html('<i class="fa fa-plus"></i>');
+            
+        }
+    else
+        {
+            $(this).html('<i class="fa fa-minus"></i>');
+        }
+});
+/*End in Dashboard plus*/
